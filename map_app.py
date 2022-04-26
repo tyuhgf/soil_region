3120ad000c38ae77aca6977891aa6f3fcab468fd
Alexey Rukhovich <tyuhgf@github.com>
2022-04-26 18:31:07 +0300

    fix MapImage buffer behavior

--- map_app.py
+++ map_app.py
@@ -444,7 +444,9 @@ class MapImage:
         if ds is None:
             return
         for (x, y, channels) in list(self._buffer_for_get_bands.keys()):
-            if self.chan_dict_rev[b] in channels:
+            if b in self.chan_dict_rev and self.chan_dict_rev[b] in channels:
+                self._buffer_for_get_bands.pop((x, y, channels))
+            if b in channels:
                 self._buffer_for_get_bands.pop((x, y, channels))
         band = ds.GetRasterBand(1)
         if b == '_map_mask_':
